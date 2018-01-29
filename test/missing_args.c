207fe2c340003df0a468053ff12cd99f4a070b03
Caleb James DeLisle <cjd@cjdns.fr>
2018-01-29 23:02:42 +0100

    Added missing-args tests

---
+++ test/missing_args.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+int callme_maybe(int arg1, int arg2, int arg3);
+int main() { printf("3 = %d", callme_maybe(1, 2, 3)); }
---
+++ test/missing_args2.c
@@ -0,0 +1,4 @@
+int callme_maybe(int arg1, int arg2)
+{
+    return arg1 + arg2;
+}
